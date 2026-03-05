50a1497d2cbfdffa5106f01fa886b69afc4a2f5e
sachaguz <sachaguz05@gmail.com>
2026-03-05 16:12:22 -0600

    Use specified

--- make_admin.py
+++ make_admin.py
@@ -1,6 +1,7 @@
 from app import app
 from models import db, User
 
+# Se usa para emergencias, el sistema por sí sólo ya puede hacerlo
 with app.app_context():
     # Cambia el correo por el del usuario que quieres hacer admin
     user = User.query.filter_by(email='sachaguz05@gmail.com').first()
@@ -8,6 +9,6 @@ with app.app_context():
     if user:
         user.es_admin = True
         db.session.commit()
-        print(f"✅ Usuario {user.nombre} ahora es administrador.")
+        print(f"Usuario {user.nombre} ahora es administrador.")
     else:
-        print("❌ Usuario no encontrado.")
+        print("Usuario no encontrado.")
